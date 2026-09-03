a2507031ab7a31c6d344bd2c964877bc3255e2b1
Nyk00s <nikodemfal@o2.pl>
2026-09-03 15:48:47 +0200

    added try/except handler for 403 forbidden action of getting messages, and fixed logic of filtering messages

--- bot/cogs/guess_user.py
+++ bot/cogs/guess_user.py
@@ -12,28 +12,33 @@ class GuessUser(commands.Cog):
 
     async def _get_filtered_messages(self, channel: discord.TextChannel, random_date: datetime) -> list[discord.Message]:
         messages = []
-        async for message in channel.history(limit=30, around=random_date):
-            if message.author.bot or message.content.startswith(('!', '/')):
-                continue
+        try:
+            async for message in channel.history(limit=30, around=random_date):
 
-            cleaned_content = message.content
-            if message.mentions:
-                for user in message.mentions:
-                    cleaned_content = cleaned_content.replace(user.mention, "")
-            if message.role_mentions:
-                for role in message.role_mentions:
-                    cleaned_content = cleaned_content.replace(role.mention, "")
+                print(f"text: {message.content}, mention: {len(message.mentions)}, attachments: {len(message.attachments)}")
+                if message.author.bot or message.content.startswith(('!', '/')):
+                    continue
 
-            has_text = bool(cleaned_content.strip())
-            has_attachments = bool(message.attachments)
+                cleaned_content = message.content
+                if message.mentions:
+                    for user in message.mentions:
+                        cleaned_content = cleaned_content.replace(user.mention, "")
+                if message.role_mentions:
+                    for role in message.role_mentions:
+                        cleaned_content = cleaned_content.replace(role.mention, "")
+                if message.mention_everyone:
+                    cleaned_content = cleaned_content.replace("@everyone", "")
+                has_text = bool(cleaned_content.strip())
+                has_attachments = bool(message.attachments)
 
-            if not has_text and not has_attachments:
-                continue
+                if not has_text and not has_attachments:
+                    continue
 
-            if len(cleaned_content) < 6:
-                continue
-
-            messages.append(message)
+                if len(cleaned_content) < 6 and not has_attachments:
+                    continue
+                messages.append(message)
+        except discord.Forbidden:
+            print("forbidden")
         return messages
 
 
@@ -59,13 +64,15 @@ class GuessUser(commands.Cog):
             random_date = start_date + (datetime.now(timezone.utc) - start_date) * random.random()
         else:
             interaction.followup.send("Algorithm didn't find any messages, or a sufficient number of unique users", ephemeral=True)
-        
-        target_message = random.choice(messages)
 
-        timeout = 20
-        view = GuessUserQuizView(authors, target_message, timeout=timeout)
-        await view.start_view(interaction)
-        await view.wait()
+        if messages:
+            target_message = random.choice(messages)
+            timeout = 20
+            view = GuessUserQuizView(authors, target_message, timeout=timeout)
+            await view.start_view(interaction)
+            await view.wait()
+        else:
+            await interaction.followup.send("Algorithm wasn't able to create a quiz. Remember that at least 3 unique people have to write something on given channel", ephemeral=True)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(GuessUser(bot))
